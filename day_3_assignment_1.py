a9470baa5376b2bccfd576b1a883c1ec9a581430
Sahil433Verma <52738114+Sahil433Verma@users.noreply.github.com>
2021-07-29 22:24:30 +0530

    Add files via upload

---
+++ day_3_assignment_1.py
@@ -0,0 +1,17 @@
+reciever_list = [2 , 1 , 5 , 3 , 4]
+# [2 , 1 , 4 , 5 , 3] 
+dict1 = {
+    (reciever_list.index(reciever_list[0])+1):reciever_list[0],
+    (reciever_list.index(reciever_list[1])+1):reciever_list[1],
+    (reciever_list.index(reciever_list[2])+1):reciever_list[2],
+    (reciever_list.index(reciever_list[3])+1):reciever_list[3],
+    (reciever_list.index(reciever_list[4])+1):reciever_list[4]
+    }
+key_list = list(dict1.keys())
+val_list = list(dict1.values())
+donor_list=[0,0,0,0,0]
+for i in range(5):
+    position = val_list.index(reciever_list[i])
+    donor_list[val_list[position]-1] = key_list[position]
+
+print(donor_list)
